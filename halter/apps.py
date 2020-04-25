13b73d151f0b2d431bf24791ce8f5c08143fc095
fatih-berktug <faatihberktug@hotmail.com>
2020-04-25 17:29:58 +0300

    Halter 25-4-2020 Commit

--- halter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HalterConfig(AppConfig):
-    name = 'halter'
--- halter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-# from wushu.Views import DashboardViews, AthleteViews, RefereeViews, ClubViews, CoachViews, DirectoryViews, UserViews,CompetitionViews, AdminViews,HelpViews,PageViews,PreRegistration
-
-app_name = 'halter'
-
-urlpatterns = []
