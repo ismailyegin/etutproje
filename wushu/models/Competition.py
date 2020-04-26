6c6541706e22173ce414e4356a6a5957818fa93e
ismailyegin <ismail@kobiltek.com>
2020-04-26 17:39:45 +0300

    competition changed

--- wushu/models/Competition.py
+++ wushu/models/Competition.py
@@ -6,17 +6,17 @@ from wushu.models.Athlete import Athlete
 from wushu.models.EnumFields import EnumFields
 
 
+
+
+
+
+
+
+
+
+
+
 class Competition(models.Model):
-    OPEN = 'Ön Kayıt Açık'
-    CLOSED = 'Ön Kayıt Tamamlandı'
-    WAITED = 'Beklemede'
-
-    STATUS_CHOICES = (
-        (OPEN, 'Ön Kayıt Açık'),
-        (CLOSED, 'Ön Kayıt Tamamlandı'),
-        (WAITED, 'Beklemede')
-    )
-
     TURKEY = 'Türkiye'
     WORLD = 'Dünya'
     OLYMPIAD = 'Olimpiyat'
@@ -39,24 +39,29 @@ class Competition(models.Model):
         (PERSONAL, 'Ferdi'),
     )
 
-    creationDate = models.DateTimeField(auto_now_add=True)
-    operationDate = models.DateTimeField(auto_now=True)
-    name = models.CharField(blank=False, null=False, max_length=1000)
-    startDate = models.DateTimeField()
-    finishDate = models.DateTimeField()
-    eventDate = models.CharField(max_length=1000, null=True, blank=True)
-    eventPlace = models.CharField(max_length=1000, null=True, blank=True)
-    compType = models.IntegerField(null=True, blank=True, choices=COMPTYPE)
-    compGeneralType = models.IntegerField(null=True, blank=True, choices=COMPGENERALTYPE)
-    juryCount = models.IntegerField(null=True, blank=True)
-    eskimi = models.BooleanField(default=False)
-    isOpen = models.BooleanField(default=False)
-    registerStartDate = models.DateTimeField()
-    registerFinishDate = models.DateTimeField()
+    compType = models.IntegerField(db_column='compType', blank=True, null=True, choices=COMPTYPE)  # Field name made lowercase.
+    creationDate = models.DateTimeField(db_column='creationDate', blank=True, null=True)  # Field name made lowercase.
+    finishDate = models.DateTimeField(db_column='finishDate', blank=True, null=True)  # Field name made lowercase.
+    kobilId = models.IntegerField(db_column='kobilId')  # Field name made lowercase.
+    name = models.CharField(max_length=255, blank=True, null=True)
+    operationDate = models.DateTimeField(db_column='operationDate', blank=True, null=True)  # Field name made lowercase.
+    startDate = models.DateTimeField(db_column='startDate', blank=True, null=True)  # Field name made lowercase.
+    eventPlace = models.CharField(db_column='eventPlace', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    eventDate = models.CharField(db_column='eventDate', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    juryCount = models.IntegerField(db_column='juryCount', blank=True, null=True)  # Field name made lowercase.
+    compGeneralType = models.IntegerField(db_column='compGeneralType', blank=True, null=True, choices=COMPGENERALTYPE)  # Field name made lowercase.
+    eskimi = models.IntegerField(blank=True, null=True)
+    isOpen = models.IntegerField(db_column='isOpen', blank=True, null=True)  # Field name made lowercase.
+    registerStartDate = models.DateTimeField(db_column='registerStartDate', blank=True, null=True)  # Field name made lowercase.
+    registerFinishDate = models.DateTimeField(db_column='registerFinishDate', blank=True, null=True)  # Field name made lowercase.
 
     def __str__(self):
         return '%s ' % self.name
 
     class Meta:
+        managed = False
+        db_table = 'competition'
         default_permissions = ()
-        db_table = "competition"
+
+
+
