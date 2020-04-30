1bcba9111cfa66a76560f02d3e7e6ab885ef997d
ismailyegin <ismail@kobiltek.com>
2020-04-30 15:21:20 +0300

    kolon isimleri güncellendi

--- wushu/models/Athlete.py
+++ wushu/models/Athlete.py
@@ -10,14 +10,13 @@ from wushu.models.Communication import Communication
 
 
 class Athlete(models.Model):
-    person = models.OneToOneField(Person, on_delete=models.CASCADE)
-    communication = models.OneToOneField(Communication, on_delete=models.CASCADE)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    person = models.OneToOneField(Person, on_delete=models.CASCADE, db_column='person')
+    communication = models.OneToOneField(Communication, on_delete=models.CASCADE, db_column='communication')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, db_column='user')
     belts = models.ManyToManyField(Level)
     licenses = models.ManyToManyField(License)
     creationDate = models.DateTimeField(auto_now_add=True)
     operationDate = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return '%s %s' % (self.user.first_name, self.user.last_name)
