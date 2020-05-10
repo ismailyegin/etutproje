13c74fdadd0ed2dfd5e4b0fb80bf33ad911fa879
ismailyegin <ismail@kobiltek.com>
2020-05-10 17:33:59 +0300

    ilk commit

--- oxiterp/urls.py
+++ oxiterp/urls.py
@@ -19,8 +19,7 @@ from django.urls import path, re_path
 from django.conf.urls import url, include
 
 from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework_simplejwt import views as jwt_views
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
