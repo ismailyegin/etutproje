d3c18706cbb2e783eea63ca70adf4895d03a543e
fatih-berktug <faatihberktug@hotmail.com>
2020-06-11 17:06:25 +0300

    pdf kaydet

--- oxiterp/settings/base.py
+++ oxiterp/settings/base.py
@@ -76,6 +76,8 @@ TEMPLATES = [
                 'django.template.context_processors.i18n',
                 'sbs.services.general_methods.getMenu',
                 'sbs.services.general_methods.getAdminMenu',
+                'sbs.services.general_methods.getPersonelMenu',
+
                 'sbs.services.general_methods.getAthleteMenu',
                 'sbs.services.general_methods.getRefereeMenu',
                 'sbs.services.general_methods.getCoachMenu',
