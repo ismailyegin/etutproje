21bc0eeae37845e1ffe4e7ab86fe061ab1b3c66f
fatih-berktug <faatihberktug@hotmail.com>
2020-09-25 16:22:55 +0300

    mesajlaşma

--- sbs/services/general_methods.py
+++ sbs/services/general_methods.py
@@ -327,12 +327,9 @@ def get_message(request):
         #         #     messages|= Message.objects.filter(pk=item)
         #         # messages=messages.distinct().order_by('-creationDate')
 
-        # sonra acılacak alan
-        # query = 'select * from (SELECT * FROM etutproje.sbs_message where user_id=%s or recipient_id=%s order by creationDate desc limit 10000) as t group by chat_id' % (
-        #     request.user.pk, request.user.pk)
-        # messages = list(Message.objects.raw(query))
-
-        messages = Message.objects.all()
+        query = 'select * from (SELECT * FROM etutproje.sbs_message where user_id=%s or recipient_id=%s order by creationDate desc limit 10000) as t group by chat_id' % (
+            request.user.pk, request.user.pk)
+        messages = list(Message.objects.raw(query))
         mesaj = []
         for item in messages:
 
@@ -379,12 +376,12 @@ def get_message(request):
                                   image,
                                   username))
 
-        # say = len(list(Message.objects.raw(
-        #     'select * from (select * from (SELECT * FROM etutproje.sbs_message where  recipient_id=%s order by creationDate desc limit 10000) as t group by chat_id) as x where x.is_show=0;' % (
-        #         request.user.pk))))
+        say = len(list(Message.objects.raw(
+            'select * from (select * from (SELECT * FROM etutproje.sbs_message where  recipient_id=%s order by creationDate desc limit 10000) as t group by chat_id) as x where x.is_show=0;' % (
+                request.user.pk))))
         return {
             'messages': mesaj,
-            # 'messageCount': say,
+            'messageCount': say,
         }
     else:
         return {}
